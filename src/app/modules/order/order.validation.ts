75e99525362e8d7dc437b516f024816ccdd8911c
Shahed <151946873+shahed-emsl@users.noreply.github.com>
2024-05-22 19:06:18 +0600

    email validation message add

--- src/app/modules/order/order.validation.ts
+++ src/app/modules/order/order.validation.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const OrderValidationSchema = z.object({
-  email: z.string().email().trim(),
+  email: z.string().email({ message: 'Email is not valid' }).trim(),
   productId: z.string().trim(),
   price: z.number(),
   quantity: z.number().min(1).default(1),
